00b9ad5c6b2b0cdd581f484f6a80121595f11681
SakuraNeko <NekoCN@outlook.com>
2019-03-23 17:17:48 +0800

    添加项目文件。

---
+++ CutPictureFrom/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CutPictureFrom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.FileName = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SaveFold = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "JPEG图像(*.jpg)|*.jpg|PNG图像(*.png)|*.png|所有文件(*.*)|*.*";
+            this.openFileDialog1.Title = "选择图片";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FileName
+            // 
+            this.FileName.AutoSize = true;
+            this.FileName.Location = new System.Drawing.Point(149, 25);
+            this.FileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(22, 24);
+            this.FileName.TabIndex = 1;
+            this.FileName.Text = " ";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // SaveFold
+            // 
+            this.SaveFold.AutoSize = true;
+            this.SaveFold.Location = new System.Drawing.Point(149, 83);
+            this.SaveFold.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SaveFold.Name = "SaveFold";
+            this.SaveFold.Size = new System.Drawing.Size(22, 24);
+            this.SaveFold.TabIndex = 2;
+            this.SaveFold.Text = " ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(522, 71);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "保存目录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 217);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "开始";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 152);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(645, 37);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "图片位置：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "输出目录：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 279);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.SaveFold);
+            this.Controls.Add(this.FileName);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "切割图片";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label FileName;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label SaveFold;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CutPictureFrom/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CutPictureFrom
+{
+    public partial class Form1 : Form
+    {
+        public static string path;
+        public static string name;
+        public static int finish;
+        public Form1()
+        {
+            InitializeComponent();
+            finish = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileName.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                SaveFold.Text = folderBrowserDialog1.SelectedPath;
+                path = SaveFold.Text;
+            }
+        }
+        public class Temp
+        {
+            public Bitmap bitmap;
+            public int index;
+            public int i;
+            public int j;
+            public int amount;
+            public Temp(Bitmap bitmap,int index,int i,int j,int amount)
+            {
+                this.bitmap = bitmap;
+                this.index = index;
+                this.i = i;
+                this.j = j;
+                this.amount = amount;
+            }
+        }
+        public static void ThreadMethod1(object obj)
+        {
+            FileStream fs = new FileStream(name, FileMode.Open, FileAccess.Read);
+            Image image = Image.FromStream(fs);
+            fs.Close();
+            fs.Dispose();
+
+            Bitmap bitmap = new Bitmap(image);
+            Temp t = obj as Temp;
+
+            image.Dispose();
+            fs.Close();
+            fs.Dispose();
+
+            int index = t.index;
+            int i = t.i;
+            int j = t.j;
+            int amount = t.amount;
+            Bitmap temp = new Bitmap(bitmap.Width / amount, bitmap.Height / amount);
+            int t1 = 0;
+            int t2 = 0;
+            for (int x = j * (bitmap.Width / amount); x < j * (bitmap.Width / amount) + bitmap.Width / amount; x++)
+            {
+                t2 = 0;
+                for (int y = i * (bitmap.Height / amount); y < i * (bitmap.Height / amount) + bitmap.Height / amount; y++)
+                {
+                    temp.SetPixel(t1, t2, bitmap.GetPixel(x, y));
+                    t2++;
+                }
+                t1++;
+            }
+
+            temp.Save(path + "\\" + amount + "X" + amount + "\\" + (index + 1) + ".png", ImageFormat.Png);
+            temp.Dispose();
+            bitmap.Dispose();
+
+            finish++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            finish = 0;
+            name = FileName.Text;
+            timer1.Start();
+            progressBar1.Maximum = 4 * 4 + 5 * 5 + 6 * 6;//决定最大切割网格
+            progressBar1.Value = 0;
+            for (int amount = 4; amount <= 6; amount++)//修改也记得改这里
+            {
+                int index = 0;
+                Directory.CreateDirectory(SaveFold.Text + "\\" + amount + "X" + amount);
+                for (int i = 0; i < amount; i++)
+                {
+                    for (int j = 0; j < amount; j++)
+                    {
+                        try
+                        {
+                            ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadMethod1), new Temp(null, index, i, j, amount));
+                            index++;
+
+                        }
+                        catch (Exception ex)
+                        {
+                            j--;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = finish;
+        }
+    }
+}
